cfd273c89903a004593c9f2f2de9a0db8e4629b5
Shuma Yoshioka <s.yoshioka@p.cartahd.com>
2025-04-21 14:02:46 +0900

    Update MyPlayerFactoryImpl.kt

--- ep2basedlib/ep2basedlib/src/styled/java/jp/s64/example/ep2basedlib/MyPlayerFactoryImpl.kt
+++ ep2basedlib/ep2basedlib/src/styled/java/jp/s64/example/ep2basedlib/MyPlayerFactoryImpl.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.view.View
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
+/**
+ * `com.google.android.exoplayer2.ui.StyledPlayerView` are built with Layout XML.
+ * - https://github.com/google/ExoPlayer/blob/dd430f70/library/ui/src/main/java/com/google/android/exoplayer2/ui/StyledPlayerView.java
+ * - https://github.com/google/ExoPlayer/blob/dd430f70/library/ui/src/main/res/layout/exo_styled_player_view.xml
+ */
 class MyPlayerFactoryImpl : MyPlayerFactory<StyledPlayerView> {
 
     override fun createView(context: Context): StyledPlayerView {
